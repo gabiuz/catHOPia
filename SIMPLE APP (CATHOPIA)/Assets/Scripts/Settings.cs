06218937b96766339564b403c6619dd8bf7b8189
miggyreyesss <167219936+miggyreyesss@users.noreply.github.com>
2024-05-05 19:57:50 +0800

    aw

---
+++ SIMPLE APP (CATHOPIA)/Assets/Scripts/Settings.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Settings : MonoBehaviour
+{
+    public void Settings_Audio()
+    {
+        SceneManager.LoadSceneAsync(6);
+    }
+}
