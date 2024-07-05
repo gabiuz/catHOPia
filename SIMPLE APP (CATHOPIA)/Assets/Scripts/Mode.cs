05d30bdfbae8921964c13cc762065daba20fedfe
avamargarette <sdballares.6@gmail.com>
2024-07-05 17:41:54 +0800

    mode

---
+++ SIMPLE APP (CATHOPIA)/Assets/Scripts/Mode.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Mode : MonoBehaviour
+{
+    public void modes_ ()
+    {
+        SceneManager.LoadSceneAsync(8);
+    }
+}
