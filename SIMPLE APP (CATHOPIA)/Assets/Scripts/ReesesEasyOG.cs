2f61de5e6318624611fb48e0304120bca0431c8e
avamargarette <sdballares.6@gmail.com>
2024-07-05 18:15:18 +0800

    hatdog

---
+++ SIMPLE APP (CATHOPIA)/Assets/Scripts/ReesesEasyOG.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ReesesEasyOG : MonoBehaviour
+{
+    public void ReesesOG ()
+    {
+        SceneManager.LoadSceneAsync(1);
+    }
+}
---
+++ SIMPLE APP (CATHOPIA)/Assets/Scripts/ReesesMediumOG.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ReesesMediumOG : MonoBehaviour
+{
+    public void ReesesMOG()
+    {
+        SceneManager.LoadSceneAsync(9);
+    }
+}
