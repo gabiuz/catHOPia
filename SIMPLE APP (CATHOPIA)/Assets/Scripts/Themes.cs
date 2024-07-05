cab6e77c94742380dbf48e332ce438f07e54a62b
avamargarette <sdballares.6@gmail.com>
2024-07-05 17:29:19 +0800

    ahahhhahahaahh

---
+++ SIMPLE APP (CATHOPIA)/Assets/Scripts/Themes.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Themes : MonoBehaviour
+{
+    public void Th_eme()
+    {
+        SceneManager.LoadSceneAsync(7);
+    }
+}
